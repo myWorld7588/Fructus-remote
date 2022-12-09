515532fa9de2c02d0e0e540e5da36f3776f2a728
myWorld7588 <jin.jakechoi@gmail.com>
2022-12-09 14:25:39 -0500

    First Initial Commit - Fruit App Data, Resources, images, Colors setup

---
+++ Fructus/View/FruitCardView.swift
@@ -0,0 +1,20 @@
+//
+//  FruitCardView.swift
+//  Fructus
+//
+//  Created by Jake Choi on 12/9/22.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FruitCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitCardView()
+    }
+}
