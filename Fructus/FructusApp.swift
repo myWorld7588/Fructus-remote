80ae8612a4bbd04738b70997a475be8702593794
myWorld7588 <jin.jakechoi@gmail.com>
2022-12-09 14:13:10 -0500

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Jake Choi on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
