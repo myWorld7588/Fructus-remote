92bb32c158978fc0c86a7d500477cf48c1753f73
myWorld7588 <jin.jakechoi@gmail.com>
2022-12-09 18:32:06 -0500

    Create an Onboardingview using the new Page Tab View

--- Fructus/View/FruitCardView.swift
+++ Fructus/View/FruitCardView.swift
@@ -8,10 +8,60 @@
 import SwiftUI
 
 struct FruitCardView: View {
+    
+    // MARK: - PROPERTY
+    
+    @State private var isAnimating: Bool = false
+    
+    // MARK: - BODY
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack(spacing: 20) {
+                // FRUIT: IMAGE
+                Image("blueberry")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 0, x: 6, y: 8)
+                    .scaleEffect(isAnimating ? 1.0 : 0.6)
+                
+                // FRUIT: TITLE
+                
+                Text("Blueberry")
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
+                
+                // FRUIT: HEADLINE
+                Text("Blueberries are sweet, nutritious and widly popular fruit all over the world.")
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                    .frame(maxWidth: 480)
+                
+                
+                // BUTTON: START
+                StartButtonView()
+            } //: VSTACK
+        } //: ZSTACK
+        .onAppear {
+            withAnimation(.easeOut(duration: 0.5)) {
+                isAnimating = true
+            }
+        }
+        
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+        .cornerRadius(20)
+        .padding(.horizontal, 20)
+        
     }
 }
+
+// MARK: - PREVIEW
+
 
 struct FruitCardView_Previews: PreviewProvider {
     static var previews: some View {
