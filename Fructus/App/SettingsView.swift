19d3745e8fac964729a52873c226918dd4d30df4
myWorld7588 <jin.jakechoi@gmail.com>
2022-12-10 20:41:18 -0500

    ...?

--- Fructus/App/SettingsView.swift
+++ Fructus/App/SettingsView.swift
@@ -42,11 +42,22 @@ struct SettingsView: View {
                     // MARK: - SECTION 2
                     
                     // MARK: - SECTION 3
+                    GroupBox(label: SettingsLabelView(labelText: "Application", labelImage: "apps.iphone")) {                        
+                        
+                        SettingRowView(name: "Developer", content: "myWorld7588")
+                        SettingRowView(name: "Designer", content: "myWorld7588")
+                        SettingRowView(name: "Compatibility", content: "iOS16")
+                        SettingRowView(name: "Website", linkLabel: "GitHub", linkDestination: "https://github.com/myWorld7588")
+                        SettingRowView(name: "Twitter")
+                        SettingRowView(name: "SwiftUI", content: "3.0")
+                        SettingRowView(name: "Version", content: "1.1.0")
+                    } //: BOX
                     
                     
                 } //: VSTACK
                 .navigationTitle("Settings")
                 .navigationBarTitleDisplayMode(.large)
+                
             } //: SCROLL
         } //: NAVIGATION
     }
---
+++ Fructus/View/SettingRowView.swift
@@ -0,0 +1,54 @@
+//
+//  SettingRowView.swift
+//  Fructus
+//
+//  Created by Jake Choi on 12/10/22.
+//
+
+import SwiftUI
+
+struct SettingRowView: View {
+    
+    // MARK: - PROPERTY
+    var name: String
+    var content: String? = nil
+    var linkLabel: String? = nil
+    var linkDestination: String? = nil
+    
+    // MARK: - BODY
+    
+    
+    var body: some View {
+        VStack {
+            Divider().padding(.vertical, 4)
+
+            
+            HStack {
+                Text(name).foregroundColor(Color.gray)
+                Spacer()
+                if (content != nil) {
+                    Text(content!)
+                } else if (linkLabel != nil && linkDestination != nil) {
+                    Link(linkLabel!, destination: URL(string: "https://\(linkDestination!)")!)
+                    Image(systemName: "arrow.up.right.square").foregroundColor(.pink)
+                }
+                
+                else {
+                    EmptyView()
+                }
+            }
+        }
+    }
+}
+
+// MARK: - PREVIEW
+
+
+struct SettingRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingRowView(name: "Developer", content: "myWorld7588")
+            .previewLayout(.fixed(width: 375, height: 60))
+            .padding()
+        
+    }
+}
